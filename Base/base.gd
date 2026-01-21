4e702063d3e3fc21505af7891d627dcd069185fc
Chad Purdy <cpurdy@flourbluffschools.org>
2026-01-21 12:19:30 -0600

    working health label

--- Base/base.gd
+++ Base/base.gd
@@ -7,12 +7,18 @@ var health : int :
 	set(new_health):
 		health = new_health
 		label_3d.text = str(health) + "/" + str(max_health)
+		var red : Color = Color.RED
+		var white : Color = Color.WHITE
+		label_3d.modulate = red.lerp(white, float(health)/float(max_health))
+		
+		
 		if health == 0:
 			get_tree().reload_current_scene()
 @onready var label_3d: Label3D = $Label3D
 
 func _ready() -> void:
 	health = max_health
+	
 
 func take_damage() -> void:
 	print("damage taken by base")
