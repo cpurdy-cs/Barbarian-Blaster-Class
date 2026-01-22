9c2ad86aeaa300687c45180b993b2353ee816b6c
Chad Purdy <cpurdy@flourbluffschools.org>
2026-01-22 12:40:36 -0600

    rotate turret wip

--- turret_manager.gd
+++ turret_manager.gd
@@ -2,8 +2,11 @@ extends Node3D
 class_name TurretManager
 
 @export var turret : PackedScene
+@export var enemy_path : Path3D
+
 
 func build_turret(position:Vector3) -> void:
 	var new_turret = turret.instantiate()
 	add_child(new_turret)
 	new_turret.global_position = position
+	new_turret.enemy_path = enemy_path
