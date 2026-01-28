d51efb9a9bfa5271fbb0c094eb7f079b91b1713a
Chad Purdy <cpurdy@flourbluffschools.org>
2026-01-28 12:26:26 -0600

    add difficulty curves

--- DifficultyManager/difficulty_manager.gd
+++ DifficultyManager/difficulty_manager.gd
@@ -2,6 +2,7 @@ extends Node
 
 @export var game_length := 30.0
 @export var spawn_time_curve : Curve
+@export var enemy_health_curve : Curve
 @onready var timer: Timer = $Timer
 
 func _ready() -> void:
@@ -16,3 +17,5 @@ func game_progress_ratio() -> float:
 func get_spawn_time() -> float:
 	return spawn_time_curve.sample(game_progress_ratio())
 	
+func get_enemy_health() -> float:
+	return enemy_health_curve.sample(game_progress_ratio())
